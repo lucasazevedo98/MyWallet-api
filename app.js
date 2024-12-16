64fb4b9e9b0d4d2b026bc46c81f865dddd2a2b7f
Lucas <azevedi22@gmail.com>
2024-12-15 21:19:43 -0300

    Ajustes finais na aplicação

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ import  dotenv from "dotenv"
 import { conexaoDB } from "./config/db.js"
 import usuariosRotas from "./routes/usuariosRotas.js"
 import transacaoRotas from "./routes/transacaoRotas.js"
+import cors from "cors"
 
 dotenv.config()
 
 const app = express()
 
 app.use(json())
+app.use(cors())
 
 
 conexaoDB()
@@ -26,5 +28,5 @@ app.use(transacaoRotas)
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT, ()=>{
-    console.log("servidor rodando!")
+    console.log(`servidor rodando! na porta ${PORT}`)
 })
